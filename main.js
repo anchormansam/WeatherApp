fe3f73ad0170ade194bf27937c2d6d62add5f7f1
Sam Wheeling <anchormansam@gmail.com>
2019-09-25 12:17:37 -0400

    updated bootstrap

--- main.js
+++ main.js
@@ -3,17 +3,26 @@ var API = ',us&appid=e1073ff107524f3183db96a3cc8ad5e3';
 var myJson={};
 var zipZip = document.getElementById('zipCode');
 const button = document.querySelector('#submit');
+var city = document.getElementById('name');
+var temp = document.getElementById('temp');
+var condition = document.getElementById('condition');
+var icon = 'http://openweathermap.org/img/wn/';
+var list ='@2x.png';
+var seasonEl = document.getElementById('season');
 
 button.addEventListener('click', zipRequest);
 
 async function getWeatherData(zip_code_from_form) {
+    // removed spaces from user code input
+    zip_code_from_form = zip_code_from_form.replace(/\s/g, '');
     var api_link = api_url + zip_code_from_form + API;
     console.log(api_link);
 
     fetch(api_link)
     .then(function(response) {
     if (!response.ok) {
-        throw new Error('HTTP error, status = ' + response.status);
+        alert("Check YO DIGITS");
+        throw new Error('HTTP error, status =' + response.status);
     }
     return response.json();
     })
@@ -24,6 +33,7 @@ async function getWeatherData(zip_code_from_form) {
         //zipZip.innerHTML = myJson["name"];
         updateTemp(myJson['main'].temp);
         updateCondition(myJson["weather"][0].description);
+        imageCondition(myJson["weather"][0].icon);
     });
 
     
@@ -31,30 +41,44 @@ async function getWeatherData(zip_code_from_form) {
 
 function zipRequest() {
     console.log("zipRequest")
-    
-    // make sure the value is proper
+     // make sure the value is proper
     getWeatherData(zipZip.value);
 }
 
 function updateCity(str) {
     // update city html element with str
-    var city = document.getElementById('name');
     city.textContent = str;
 
 }
-console.log(updateCity)
+// console.log(updateCity)
 
 function updateTemp(str) {
     // update temp html element with str
-    var temp = document.getElementById('temp');
     temp.textContent = str;
+    celsius(str);
 
 }
 
 function updateCondition(str) {
     // update Condition html with str
-    var condition = document.getElementById('condition');
     condition.textContent = str;
 }
 
+function celsius(updateCelsius) {
+    var newCelsius = Math.round((updateCelsius - 273.15));
+    console.log(newCelsius);
+    fahrenheit(newCelsius);
+    document.getElementById('Celsius').innerHTML = newCelsius;
+}
 
+function fahrenheit(newCelsius) {
+    var newFahrenheit = Math.round(((newCelsius) * 9/5) + 32);
+    console.log(newFahrenheit);
+    document.getElementById('Fahrenheit').innerHTML = newFahrenheit;
+}
+
+function imageCondition (updateimage) {
+    var imageLink = icon + updateimage + list;
+    // upate html src
+    seasonEl.src = imageLink;
+}
